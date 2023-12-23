c4f19699d82b43f624adf47943ebcab841c07234
Mary <mariavnuzhdina@yandex.ru>
2023-12-23 13:30:28 +0300

    добавили dats.js

---
+++ data.js
@@ -0,0 +1,30 @@
+export const dataInfo = `[
+    {
+"id": "001",
+"name": "Tom_Ford",
+"gender": "для женщин",
+"country": "США",
+"image": "https://jean-batiste.ru/image/cache/data-brands-tom-ford-tom-ford-jasmine-rouge-1-800x800.jpg"
+    },
+{
+"id": "002",
+"name": "Vertus",
+"gender": "унисекс",
+"country": "Франция",
+"image": "https://cdn1.ozone.ru/s3/multimedia-i/6303328494.jpg"
+    },
+{
+ "id": "003",
+"name": "Byredo",
+"gender": "для женщин",
+"country": "Швеция",
+"image": "https://basket-09.wb.ru/vol1271/part127151/127151568/images/big/1.jpg"
+    },
+{
+ "id": "004",
+ "name": "Chanel",
+"gender": "для мужчин",
+"country": "Франция",
+"image": "https://myparfume.ru/upload/iblock/746/68lyvzrt6j3bap1zppkshl3m486euisd.jpg"
+    }
+]`
